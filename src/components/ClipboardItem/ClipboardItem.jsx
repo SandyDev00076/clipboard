1201c7df2791dbfd7ff1991598172aa83d12e823
Sanjeet Tiwari <Sanjeet.Tiwari@ecs.co.uk>
2020-02-02 03:40:10 +0530

    added pre tag in clipboard item

--- src/components/ClipboardItem/ClipboardItem.jsx
+++ src/components/ClipboardItem/ClipboardItem.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import css from "./ClipboardItem.module.scss";
 
 const ClipboardItem = ({ item }) => {
-  return <div className={css.clipboard_item}>{item.text}</div>;
+  return (
+    <div className={css.clipboard_item}>
+      <pre className={css.itemtext}>{item.text}</pre>
+    </div>
+  );
 };
 
 export default ClipboardItem;
