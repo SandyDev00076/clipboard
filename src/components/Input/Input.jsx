0e47345eba31777e4c181afcb36e92a6130e0b2c
Sanjeet Tiwari <Sanjeet.Tiwari@ecs.co.uk>
2020-02-04 21:39:06 +0530

    changed the placeholder color

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,7 +4,15 @@ import css from "./Input.module.scss";
 
 const Input = ({ rows, className, ...props }, ref) => {
   let classes = `${css.input} ${className}`;
-  return <textarea rows={rows} {...props} ref={ref} className={classes} />;
+  return (
+    <textarea
+      spellCheck={false}
+      rows={rows}
+      {...props}
+      ref={ref}
+      className={classes}
+    />
+  );
 };
 
 export default forwardRef(Input);
