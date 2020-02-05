e35cda13eea3b3e3aabea2643cc0ff80fc0422b0
Sanjeet Tiwari <Sanjeet.Tiwari@ecs.co.uk>
2020-02-06 00:26:02 +0530

    added logs to let users know the policy

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ import { faCheckCircle, faTrash } from "@fortawesome/free-solid-svg-icons";
 library.add(faCheckCircle, faTrash);
 
 function App() {
+  myLogs();
   return (
     <>
       <ClipBoard />
     </>
   );
+
+  function myLogs() {
+    console.log("Hello");
+    console.log("My name is Sanjeet Tiwari");
+    console.log("Thank you for using my website");
+    console.log(
+      "This application in NO WAY shares your clipboard data with anyone. Data is stored and retrieved from your browser's local storage."
+    );
+  }
 }
 
 export default App;
